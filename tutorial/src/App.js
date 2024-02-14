6fbf01579415dd08c511f0ac5bf1651e77c3b857
amaan35 <amaanlm35@gmail.com>
2024-02-14 18:15:55 +0530

    first of all, removing strict mode in index js, creating observable using creation function fromEvent, uses button as target and click as event type, subscribing a callback function to it and consoling a text

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import "./App.css";
+import {useEffect} from 'react'
+import { fromEvent } from "rxjs";
 
 function App() {
+  useEffect(() => {
+
+    // using fromEvent to create an observable attached with a button(producer),
+    // which emits event/data as event type click, i.e. every click on this button
+    // can be considered as data emitted by producer and subscribing to it using subscribe function
+    // of observable returned by fromEvent, here we have no need to store the returned observable so
+    // i just apply subscribe function on fromEvent 
+    const btn = document.getElementById("button1");
+    if (btn) {
+      //using javascript to react to event
+      // btn.addEventListener("click", () => console.log("clicked"));
+      fromEvent(btn, 'click').subscribe(()=>console.log('clicked using fromEvent'))
+    }
+  }, []);
+
   return (
     <div className="App">
       <h1>React and rxJS tutorial</h1>
+      <button id="button1">button1</button>
+      <br />
+      <button id="button2">button2</button>
     </div>
   );
 }
