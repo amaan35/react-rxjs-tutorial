00632ed32c51973aed24d41e591b921d6407add7
amaan35 <amaanlm35@gmail.com>
2024-02-14 18:37:57 +0530

    creating an observer and passing it to the subscribe function

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -3,8 +3,19 @@ import {useEffect} from 'react'
 import { fromEvent } from "rxjs";
 
 function App() {
+  // defining an observer with next, error and complete functions
+  const observer = {
+    next: function(event) {
+      console.log(event.target.value)
+    },
+    error: function(error){
+      console.log(error)
+    },
+    complete: function(){
+      console.log('complete')
+    }
+  }
   useEffect(() => {
-
     // using fromEvent to create an observable attached with a button(producer),
     // which emits event/data as event type click, i.e. every click on this button
     // can be considered as data emitted by producer and subscribing to it using subscribe function
@@ -14,7 +25,9 @@ function App() {
     if (btn) {
       //using javascript to react to event
       // btn.addEventListener("click", () => console.log("clicked"));
-      fromEvent(btn, 'click').subscribe((event)=>console.log('value of event : ', event.target.value))
+
+      // passing our observer in the subscribe funtion
+      fromEvent(btn, 'click').subscribe(observer)
     }
   }, []);
 
