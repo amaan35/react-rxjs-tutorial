81fc516c8dd396d847682e1659f6c0045ec8118b
amaan35 <amaanlm35@gmail.com>
2024-02-14 19:04:44 +0530

    Creating an observable without the use of creation functions like fromEvent and subscribing our observer to it, calling next callback of our observer

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,42 +1,33 @@
 import "./App.css";
-import {useEffect} from 'react'
-import { fromEvent } from "rxjs";
+import { Observable } from "rxjs";
 
 function App() {
   // defining an observer with next, error and complete functions
   const observer = {
-    next: function(event) {
-      console.log(event.target.value)
+    next: function (value) {
+      console.log(value);
     },
-    error: function(error){
-      console.log(error)
+    error: function (error) {
+      console.log(error);
     },
-    complete: function(){
-      console.log('complete')
-    }
-  }
-  useEffect(() => {
-    // using fromEvent to create an observable attached with a button(producer),
-    // which emits event/data as event type click, i.e. every click on this button
-    // can be considered as data emitted by producer and subscribing to it using subscribe function
-    // of observable returned by fromEvent, here we have no need to store the returned observable so
-    // i just apply subscribe function on fromEvent 
-    const btn = document.getElementById("button1");
-    if (btn) {
-      //using javascript to react to event
-      // btn.addEventListener("click", () => console.log("clicked"));
+    complete: function () {
+      console.log("complete");
+    },
+  };
 
-      // passing our observer in the subscribe funtion
-      fromEvent(btn, 'click').subscribe(observer)
-    }
-  }, []);
+  // creating our observable without creation functions like fromEvent as we did before
+  const observable = new Observable((obs)=>obs.next('next called')).subscribe(observer);
 
   return (
     <div className="App">
       <h1>React and rxJS tutorial</h1>
-      <button id="button1" value='button1 clicked'>button1</button>
+      <button id="button1" value="button1 clicked">
+        button1
+      </button>
       <br />
-      <button id="button2" value='button2 clicked'>button2</button>
+      <button id="button2" value="button2 clicked">
+        button2
+      </button>
     </div>
   );
 }
